39ab4f367c0bd6855e609c0fa50e1b48d4cb0c80
fioki <kid.fioki@gmail.com>
2025-03-20 19:46:34 -0400

    add personalization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import starlight from '@astrojs/starlight';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://fioki.net',
-    base: '/'
+  site: 'https://fioki.net',
+  base: '.',
+  integrations: [starlight({
+    title: 'fioki',
+    })]
 });
